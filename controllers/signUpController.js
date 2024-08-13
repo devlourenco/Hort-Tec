26aa1fc3f811c3d053f4a9bd52fac6d61833dc0a
devlourenco <guilherme.gsldev@gmail.com>
2024-08-13 20:09:29 -0300

    created in ETEC, server running, to do a requests and routes tests

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -4,53 +4,53 @@ import { error } from "console";
 
 const routes = express.Router();
 
-export default function signUpController() {
-  routes.get("/cadastro", async (req, res) => {
-    const userSaves = await service.listUser();
+routes.get("/", async (req, res) => {
+  const userSaves = await service.listUser();
 
-    if (userSaves.length < 1) {
-      return res.status(401).end();
-    }
-    if (error) {
-      console.log({ message: error });
-    }
-    res.status(200).send({ message: userSaves });
-  });
+  if (userSaves.length < 1) {
+    return res.status(401).end();
+  }
+  if (error) {
+    console.log({ message: error });
+  }
+  res.status(200).send({ message: userSaves });
+});
 
-  routes.post("/", async (req, res) => {
-    const { email, senha, nome, tipoUsuario } = req.body;
+routes.post("/", async (req, res) => {
+  const { email, senha, nome, tipoUsuario } = req.body;
 
-    if (senha.length < 6) {
-      return res.status(400).send({ message: "Senha curta demais" });
-    }
-    if (error) {
-      console.log({ message: error });
-    }
+  if (senha.length < 6) {
+    return res.status(400).send({ message: "Senha curta demais" });
+  }
+  if (error) {
+    console.log({ message: error });
+  }
 
-    await service.createUser(email, senha, nome, tipoUsuario);
+  await service.createUser(email, senha, nome, tipoUsuario);
 
-    return res.status(200).send({ message: "cadastro realizado!" });
-  });
+  return res.status(200).send({ message: "cadastro realizado!" });
+});
 
-  routes.put("/", async (req, res) => {
-    const { email, senha, nome, tipoUsuario } = req.body;
+routes.put("/", async (req, res) => {
+  const { email, senha, nome, tipoUsuario } = req.body;
 
-    if (error) {
-      console.log({ message: error });
-    }
+  if (error) {
+    console.log({ message: error });
+  }
 
-    await service.updateUser(email, senha, nome, tipoUsuario);
+  await service.updateUser(email, senha, nome, tipoUsuario);
 
-    return res.status(200).send({ message: "Erro ao editar usuário" });
-  });
+  return res.status(200).send({ message: "Erro ao editar usuário" });
+});
 
-  routes.delete("/:idUser", async (req, res) => {
-    const { idUser } = req.params;
-    if (idUser == null) {
-      return res.status(400).send({ message: "id not informated" });
-    }
-    await service.deleteUser(idUser);
+routes.delete("/:idUser", async (req, res) => {
+  const { idUser } = req.params;
+  if (idUser == null) {
+    return res.status(400).send({ message: "id not informated" });
+  }
+  await service.deleteUser(idUser);
 
-    return res.status(200).send({ message: "Usuário deletado com sucesso" });
-  });
-}
+  return res.status(200).send({ message: "Usuário deletado com sucesso" });
+});
+
+export default routes
--- routes.js
+++ routes.js
@@ -4,6 +4,6 @@ import signUpController from "./controllers/signUpController.js";
 
 const routes = express();
 
-routes.use("/cadastro", signUpController());
+routes.use("/cadastro", signUpController);
 
 export default routes
