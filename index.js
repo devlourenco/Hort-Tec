25a87811de6ec507f98acf054c5fcb775830a9bd
devlourenco <guilherme.gldev@gmail.com>
2024-08-15 14:33:14 -0300

    Testes de requisições de cadastro feitas e rotas ocorrendo perfeitamente

--- index.js
+++ index.js
@@ -5,37 +5,7 @@ const app = express();
 app.use(express.json());
 app.use("/", routes);
 
-app.get("/teste", () => {
-  try {
-    console.log("Teste de rota get");
-  } catch (error) {
-    console.log(error);
-  }
-});
 
-app.post("/teste", () => {
-  try {
-    console.log("Teste de rota post");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.put("/teste", () => {
-  try {
-    console.log("Teste de rota put");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.delete('/teste', () => {
-  try {
-    console.log("Teste de rota delete");
-  } catch (error) {
-    console.log(error);
-  }
-})
 
 const port = 3333;
 app.listen(port, () => {
