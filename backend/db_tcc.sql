25e27d3dac44c0d86dc8a240584297735c3b6974
Mariana Lima <mariana.lima118@outlook.com>
2024-09-15 20:06:33 -0300

    Backend: alteração nas tabelas

--- backend/db_tcc.sql
+++ backend/db_tcc.sql
@@ -2,41 +2,45 @@ create database bd_hortec2;
 use bd_hortec2;
 
 create table usuarios (
-id int auto_increment primary key,
-nome varchar (50),
-email varchar (100) unique,
-senha varchar (50),
-tipo_usuario varchar (10),
-deletado boolean default false
+  id int auto_increment primary key,
+  nome varchar (50),
+  email varchar (100) unique,
+  senha varchar (50),
+  tipo_usuario varchar (10),
+  deletado boolean default false
 );
 
-create table arduinos (
+create table usuario_arduino(
+  id int auto_increment primary key,
+  usuario_id int not null unique,
+  arduino_id int not null unique,
+  nome varchar (50),
+  umidade_ideal decimal (3,2),
+  temperatura_ideal decimal (3,2), 
+)
 
-id int auto_increment primary key,
-nome_arduino varchar(50),
-usuario_id int,
-foreign key (usuario_id) references usuarios(id)
+create table arduinos (
+  id int auto_increment primary key,
+  nome_arduino varchar(50)
 );
 
 
 create table leituras (
-id int auto_increment primary key,
-arduino_id int,
-data_hora datetime,
-mensagem text,
-umidade decimal (3,2),
+  id int auto_increment primary key,
+  arduino_id int,
+  data_hora datetime,
+  mensagem text,
+  umidade decimal (3,2),
 
-foreign key (arduino_id) references arduinos(id)
+  foreign key (arduino_id) references arduinos(id)
 );    
 
 
 create table plantas (
-id int auto_increment primary key,
-nome varchar (50),
-umidade_ideal decimal (3,2),
-temperatura_ideal decimal (3,2),
-arduino_id int,
-foreign key (arduino_id) references arduinos(id)    
+  id int auto_increment primary key,
+  nome varchar (50),
+  umidade_ideal decimal (3,2),
+  temperatura_ideal decimal (3,2), 
 );
    
    
