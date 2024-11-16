79ae1806ead96d96596c857b4edeb04c2944f14c
Mariana Lima <mariana.lima118@outlook.com>
2024-11-16 19:35:50 -0300

    Backend: Alterações no banco de dados(incluida tabela de leituras com relacionamento com usuario_arduino, incluida coluna status a tabela usuario_arduino)

--- backend/db_tcc.sql
+++ backend/db_tcc.sql
@@ -9,18 +9,6 @@ create table usuarios (
   deletado boolean default false
 );
 
-create table usuario_arduino(
-  id int auto_increment primary key,
-  usuario_id int not null,
-  arduino_id int not null,
-  nome varchar (50),
-  umidade_ideal decimal,
-  temperatura_ideal decimal,
-  
-  foreign key (arduino_id) references arduinos(id),
-  foreign key (usuario_id) references usuarios(id) 
-);
-
 create table arduinos (
   id int auto_increment primary key,
   nome_arduino varchar(50)
@@ -32,30 +20,29 @@ INSERT INTO arduinos (nome_arduino) VALUES
 ('teste03'), 
 ('teste04'), 
 ('teste05'), 
-('teste06')
+('teste06');
+
+create table usuario_arduino(
+  id int auto_increment primary key,
+  usuario_id int not null,
+  arduino_id int not null,
+  nome varchar (50),
+  umidade_ideal decimal,
+  temperatura_ideal decimal,
+  status varchar(20) default 'umidade normal',
+  
+  foreign key (arduino_id) references arduinos(id),
+  foreign key (usuario_id) references usuarios(id) 
+);
 
 create table leituras (
   id int auto_increment primary key,
-  arduino_id int,
-  data_hora datetime,
-  mensagem text,
+  usuario_arduino_id int,
+  data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
   umidade decimal (3,2),
+  status varchar(20),
 
-  foreign key (arduino_id) references arduinos(id)
+  foreign key (usuario_arduino_id) references usuario_arduino(id)
 );    
 
-create table plantas (
-  id int auto_increment primary key,
-  nome varchar (50),
-  umidade_ideal decimal (3,2),
-  temperatura_ideal decimal (3,2), 
-);
-   
-   
-  
-
-
-   
-   
-
-
+/* status = umidade normal / umidade baixa */
